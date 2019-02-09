5e7eda804c4c3a061f42ff5d2f50c72d90ca838b
james medina <jamesmedina7787@outlook.com>
2019-02-09 10:23:04 -0500

    trying to set up the contact page worked out all the errors but it still doesnt work

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const ejs = require('ejs')
 // const cookieParser=require('cookie-parser')
 const bodyParser = require('body-parser')
 const app = express()
+const handlebars = require('express-handlebars')
+const mailer = require('nodemailer')
 const PORT = process.env.PORT || 3000
 let request = require('request');
 
@@ -72,6 +74,64 @@ app.get("/licenseRating", (req, res)=>{
 app.get("/contact-us", (req, res)=>{
   res.render('contact-us')
 })
+
+
+
+
+app.post('/send', (req,res)=>{
+
+  const output = `
+  <p class="message">You have a new contact request</p>
+  <h3 class="list-heading">Contact Details</h3>
+  <ul>
+    <li class="listed">${req.body.name}</li>
+    <li class="listed">${req.body.email}</li>
+    <li class="listed">${req.body.phone}</li>
+  </ul>
+  <h3 class="literature">${req.body.message}</h3>
+  `
+  async function main(){
+
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  let account = await nodemailer.createTestAccount();
+  let transporter = mailer.createTransport({
+    host: "mbox.freehostia.com",
+    port: 2525,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: 'james@eastchesterappliancecenter.net', // generated ethereal user
+      pass: '' // generated ethereal password
+    },
+    tls:{
+      rejectUnauthorized:false
+    }
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+    from: '"Nodemailer Contact" <james@eastchesterappliancecenter.net>', // sender address
+    to: "james@eastchesterappliancecenter.net", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: output // html body
+  };
+
+  // send mail with defined transport object
+  let info = await
+  transporter.sendMail(mailOptions)
+
+  console.log("Message sent: %s", info.messageId);
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", mailer.getTestMessageUrl(info));
+   // var msg = {message:'thank you'}
+}
+  res.render('contact-us')
+})
+
+
+
+
 app.get("/pilot-supply-kit", (req, res)=>{
   res.render('pilot-supply-kit')
 })
